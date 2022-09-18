7727344886ff506c79dafe32e13e8d0a8631873f
muhamad waqas <93639960+Muhammad-AdapptLimited@users.noreply.github.com>
2022-09-19 00:30:05 +0500

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,2 +1,2 @@
-import { View, ViewPropTypes as RNViewPropTypes } from 'react-native';
+import { View, ViewPropTypes as RNViewPropTypes } from 'deprecated-react-native-prop-types';
 export const ViewPropTypes = RNViewPropTypes || View.propTypes;
